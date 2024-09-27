b063ca7463597f2de3ea6d4f5ef639096364ced4
HashemJalloul <jalloulhashem@gmail.com>
2024-09-27 20:11:46 +0400

    add files

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
-let firstName = "hashem";
-let lastName = " jalloul";
-let age = 31;
-let city = "latakia";
-let country = "syria";
+// let firstName = "hashem";
+// let lastName = " jalloul";
+// let age = 31;
+// let city = "latakia";
+// let country = "syria";
 
-let hashemInfo =
-    firstName +
-    lastName +
-    "and his age: " +
-    age +
-    " he lives in: " +
-    city +
-    " in " +
-    country;
+// let hashemInfo =
+//     firstName +
+//     lastName +
+//     "and his age: " +
+//     age +
+//     " he lives in: " +
+//     city +
+//     " in " +
+//     country;
 
-let hashemInfo2 = `${firstName} ${lastName} and his age:  ${age} he lives in: ${city} in ${country}`;
+// let hashemInfo2 = `${firstName} ${lastName} and his age:  ${age} he lives in: ${city} in ${country}`;
 
-let hashemInfo2Object = {
-    firstName: "hashem",
-    lastName: " jalloul",
-    age: 31,
-    city: "latakia",
-    country: "syria",
-};
+// let hashemInfo2Object = {
+//     firstName: "hashem",
+//     lastName: " jalloul",
+//     age: 31,
+//     city: "latakia",
+//     country: "syria",
+// };
 
-let hashemInfo2FormObject = `${hashemInfo2Object.firstName} ${hashemInfo2Object.lastName} and his age is : ${hashemInfo2Object.age} he lives in ${city} in ${hashemInfo2Object.country}`;
+// let hashemInfo2FormObject = `${hashemInfo2Object.firstName} ${hashemInfo2Object.lastName} and his age is : ${hashemInfo2Object.age} he lives in ${city} in ${hashemInfo2Object.country}`;
 // console.log(hashemInfo2FormObject);
 
-let students = [
-    "hashem0",
-    "hashem1",
-    "hashem2",
-    "hashem3",
-    "hashem4",
-    "hashem5",
-];
+// let students = [
+//     "hashem0",
+//     "hashem1",
+//     "hashem2",
+//     "hashem3",
+//     "hashem4",
+//     "hashem5",
+// ];
 
-let text = "";
+// let text = "";
 
 // for (let index = 0; index < students.length; index++) {
 //     text = text + " " + students[index];
@@ -82,7 +82,6 @@ let text = "";
 
 // cratAnewElement("hello from js");
 
-
 // function cratAnewElement(text) {
 
 //     let createdElement = document.createElement('div');
@@ -90,30 +89,110 @@ let text = "";
 //     body.appendChild(createdElement);
 //     createdElement.innerHTML = text;
 
-
-
 // }
 // cratAnewElement("hello from js")
 
+// let programmingLanguages = [
+//     "c++",
+//     "c",
+//     "js",
+//     "php",
+//     "java"
+// ]
 
-let programmingLanguages = [
-    "c++",
-    "c",
-    "js",
-    "php",
-    "java"
-]
+// <td>
+//     <button onclick="deleteButtons(event)" class="btn btn-danger delete-button">
+//     Delete Row</button>
+// </td>
 
-function language(lang) {
+// function language(lang) {
+
+//     let createdElement = document.createElement('div');
+//     let body = document.querySelector('body');
+//     body.appendChild(createdElement);
+//     createdElement.innerHTML = `This language is ${lang}`
+
+// }
+// programmingLanguages.forEach((item) => {
+//     language(item)
+// })
+
+// const print = () => {
+//     console.log(2);
+
+// }
+
+// print()
+
+const addRowToTable = (name, last, handle, elementIndex) => {
+    const tableBody = document.querySelector(".table tbody");
+
+    tableBody.insertAdjacentHTML(
+        "beforeend",
+        `
+        
+                    <tr>
+                        <th scope="row">${elementIndex}</th>
+                        <td>${name}</td>
+                        <td>${last}</td>
+                        <td>${handle}</td>
+                        
+                        <td>
+                            <button class="btn btn-danger delete-button-${elementIndex}">
+                            Delete Row</button>
+                        </td>
+                    </tr>
+        `
+    );
+};
+
+let elementIndex = 0;
+
+const newRowButton = document.querySelector(".add-new-button");
+
+newRowButton.addEventListener("click", () => {
+
+    const nameInput = document.querySelector(".name");
+    const lastInput = document.querySelector(".last");
+    const handleInput = document.querySelector(".handle");
+
+    addRowToTable(nameInput.value, lastInput.value, handleInput.value, elementIndex);
+
+    const deleteButton = document.querySelector(`.delete-button-${elementIndex}`)
+
+    deleteButton.addEventListener("click", (event) => {
+
+        event.target.parentElement.parentElement.remove()
+
+    })
+    elementIndex += 1
+
+    nameInput.value = '';
+    lastInput.value = "";
+    handleInput.value = ""
+});
+
+// const nameInput = document.querySelector('.name')
+
+// nameInput.addEventListener("input", (event) => {
+
+//     const data = event.target.value
+
+//     console.log(data);
+
+// })
 
 
-    let createdElement = document.createElement('div');
-    let body = document.querySelector('body');
-    body.appendChild(createdElement);
-    createdElement.innerHTML = `This language is ${lang}`
+// const deleteButtons = (event) => {
+//     const deleteButton = document.querySelector('.delete-button')
+
+//     deleteButton.addEventListener('click', () => {
 
 
-}
-programmingLanguages.forEach(function (item) {
-    language(item)
-})
+//         event.target.parentElement.parentElement.remove();
+
+//     })
+
+//     event.target.parentElement.parentElement.remove();
+
+// }
